355b0db77a7c09a71540643c357fb2232a03d7b8
Konrad <fedek6@gmail.com>
2025-03-14 15:34:17 +0100

    Modify default port

--- backend-team-selector/server.js
+++ backend-team-selector/server.js
@@ -90,4 +90,4 @@ app.get("/members", (req, res) => {
   );
 });
 
-app.listen(4000, () => console.log("Server started on port 4000"));
+app.listen(4003, () => console.log("Server started on port 4003"));
